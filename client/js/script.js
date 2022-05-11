89781fd494d96b8a688831fa6004ef3df91d3b4d
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-11 15:34:44 +0100

    add more to script.js for submit button functions

--- client/js/script.js
+++ client/js/script.js
@@ -1,20 +1,35 @@
 // search button script
+// index.html
 const btn1 = document.querySelector('#btn-1')
 const btn2 = document.querySelector('#btn-2')
 const searchBar = document.querySelector('#search-bar')
 
+// results.html
+const targetElement = document.querySelector('ul')
+
+// important bits
 const port = 3000
-const target = "movies"
+const source = "allfilms"
 
 btn1.addEventListener('submit',btn1Fetch)
 btn2.addEventListener('submit',btn2Fetch)
 
 // Returns 10 results
 function btn1Fetch(){
-    fetch(`http://localhost:${port}/${target}`)
+    fetch(`http://localhost:${port}/${source}`)
     .then(response => response.json())
     .then(data => {
-        // do soething with searchBar.textContent...
+        try {        // do something with searchBar.textContent...
+        for (let i = 0;i < 10;i++){
+            let newLi = document.createElement('li')
+            newLi.textContent = data[i]
+            targetElement.appendChild(newLi)
+        }
+    } catch (err) {
+        let newLi = document.createElement('li')
+        newLi.textContent = "Something when wrong!"
+        targetElement.appendChild(newLi)
+    }
         console.log(data)
     });
 }
@@ -24,7 +39,17 @@ function btn2Fetch(){
     fetch(`http://localhost:${port}/${target}`)
     .then(response => response.json())
     .then(data => {
-        // do soething with searchBar.textContent...
+        // do something with searchBar.textContent...
+        try {
+            let newLi = document.createElement('li')
+            newLi.textContent = data[i]
+            targetElement.appendChild(newLi)
+        }
+        catch(err) {
+            let newLi = document.createElement('li')
+            newLi.textContent = "Something when wrong!"
+            targetElement.appendChild(newLi)
+        }
         console.log(data)
     });
 }
