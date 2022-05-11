104625195063ac0f31df9477fa0e80a4ba2f7a6e
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-11 15:21:23 +0100

    add comments to server/app.js

--- server/app.js
+++ server/app.js
@@ -14,8 +14,10 @@ console.log('We are live on ' + port)})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+// route for all films
 app.get('/allfilms', (req, res) => res.send(data))
 
+// route for specific film
 app.get('/allfilms/:id', (req, res) => {
     if (req.params.id >= movies.length || req.params.id < 1) {
       res.send(`That does not exist, please choose a number betwen 1 and ${movies.length}`)
