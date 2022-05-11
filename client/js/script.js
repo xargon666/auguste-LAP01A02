4c1eb8fb8372b75c746e44aaae4ff909f070a156
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-11 15:08:16 +0100

    add fetch functions to client

--- client/js/script.js
+++ client/js/script.js
@@ -1,1 +1,26 @@
 // search button script
+const btn1 = document.querySelector('#btn-1')
+const btn2 = document.querySelector('#btn-2')
+const port = 3000
+const target = "movies"
+
+btn1.addEventListener('submit',btn1Fetch)
+btn2.addEventListener('submit',btn2Fetch)
+
+// Returns 10 results
+function btn1Fetch(){
+    fetch(`http://localhost:${port}/${target}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+}
+
+// Returns 1 result
+function btn2Fetch(){
+    fetch(`http://localhost:${port}/${target}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+}
