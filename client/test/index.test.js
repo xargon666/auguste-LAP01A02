c9d84d7d01436afefd523c62fe8612c0956ad37e
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-13 10:57:38 +0100

    clean up files in client, reduced down to one working HTML test

--- client/test/index.test.js
+++ client/test/index.test.js
@@ -1,10 +1,14 @@
+/**
+* @jest-environment jsdom
+*/
+
 const fs = require("fs");
 const path = require("path");
 const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 
 describe("Testing Client-Side index.html...", () => {
   beforeAll(() => {
-    document.documentElement.innerHTML = html.toString(); // ???
+    document.documentElement.innerHTML = html.toString();
     console.log("Testing server is operational");
   });
 
@@ -12,23 +16,5 @@ describe("Testing Client-Side index.html...", () => {
     let docTitle = document.title;
     expect(docTitle).not.toBe("");
   });
-
-  it("Check anchor tags have href attribute", () => {
-    aVars = document.querySelectorAll("a");
-    let i = 0;
-    aVars.forEach((ele) => {
-      console.log(i, "- Anchor Tag Text:", ele.textContent);
-      expect(ele.href).toBeTruthy();
-      i++;
-    });
-  });
-  it("Check anchor tags have a href value", () => {
-    aVars = document.querySelectorAll("a");
-    let i = 0;
-    aVars.forEach((ele) => {
-      console.log(i, "- Anchor Tag Text:", ele.textContent);
-      expect(ele.href).replace("http://localhost").not.toBe("");
-      i++;
-    });
-  });
+  
 }); // END OF DESCRIBE
