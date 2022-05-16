f8da92a9aa275b30334dafdce3480ea98c4e511b
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-05-16 16:02:33 +0100

    remove package-lock.json from ignore, try new testing for anchor tags but it fails

--- client/test/index.test.js
+++ client/test/index.test.js
@@ -1,7 +1,6 @@
 /**
-* @jest-environment jsdom
-*/
-
+ * @jest-environment jsdom
+ */
 const fs = require("fs");
 const path = require("path");
 const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
@@ -9,12 +8,25 @@ const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 describe("Testing Client-Side index.html...", () => {
   beforeAll(() => {
     document.documentElement.innerHTML = html.toString();
-    console.log("Testing server is operational");
+    console.log("Testing is running");
   });
 
   it("check title has value", () => {
     let docTitle = document.title;
     expect(docTitle).not.toBe("");
   });
-  
-}); // END OF DESCRIBE
+
+  describe("header", () => {
+    describe.each(Array.from(document.querySelectorAll('header>a'))
+    )("anchor tag #%#", a => {
+        test("has a href attribute", () => {
+            expect(a.hasAttribute("href")).toBe(true);
+        });
+        
+        test("has text content", () => {
+            expect(a.textContent).toBeTruthy();
+        });
+    });
+});
+
+}); // END OF MAIN DESCRIBE
